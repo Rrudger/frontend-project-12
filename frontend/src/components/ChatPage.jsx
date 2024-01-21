ec8477ac5c057ca350376937c8baab8954a05bf7
Rrudger <soulo@yandex.ru>
2024-01-21 16:57:06 +0100

    update

--- frontend/src/components/ChatPage.jsx
+++ frontend/src/components/ChatPage.jsx
@@ -155,7 +155,7 @@ useEffect(() => {
               <span className='me-1'>#</span>
               {channel.name}
             </Button>) : (
-            <Dropdown  className='w-100 rounded-0 text-start'  as={ButtonGroup}>
+            <Dropdown  className='w-100 rounded-0 text-truncate text-start'  as={ButtonGroup}>
               <Button onClick={() => setCurrentId(channel.id)} className='w-100 rounded-0 text-start' variant={channel.id === data.currentChannelId && 'secondary'}>
                 <span className='me-1'>#</span>
                 {channel.name}
