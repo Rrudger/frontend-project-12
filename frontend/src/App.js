0e4ec5ff92b2f4a388e5c6a638ca21b0a2dd8651
Rrudger <soulo@yandex.ru>
2024-01-22 16:42:30 +0100

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,14 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { io } from 'socket.io-client';
 import Page404 from './components/Page404.jsx';
 import SingupPage from './components/SingupPage.jsx';
 import InitialPage from './components/InitialPage.jsx';
 import LoginContext from './contexts';
 import Chat from './components/Chat.jsx';
 import NavbarHeader from './components/Navbar.jsx';
-import { io } from 'socket.io-client';
 import { actions as globalActions } from './slices/globalSlice.js';
-//import i18n from './i18n';
-
 
 const App = () => {
   const ContextProvider = ({ children }) => {
@@ -20,38 +18,32 @@ const App = () => {
   return (
     <LoginContext.Provider value={{ count, setCount }}>
       {children}
-
     </LoginContext.Provider>
   );
 };
 
 const lang = useSelector((state) => state.langState.language);
-//console.log(lang);
-//console.log(i18n.language)
-  useEffect(() => {
-  }, [lang]);
+useEffect(() => {}, [lang]);
 
-  //const lastAddedChannel = useSelector((state) => state.globalState.lastChannelAddedBy);
-  //console.log(lastAddedChannel)
-  const dispatch = useDispatch();
-  const socket = io();
-  socket.on('newMessage', (payload) => {
-      dispatch(globalActions.addMessage(payload));
-      const messageBox = document.getElementById('messages-box');
-      messageBox.scrollTop = messageBox.scrollHeight;
-  });
-  socket.on('newChannel', (payload) => {
-    dispatch(globalActions.addChannel(payload))
-  });
-  socket.on('removeChannel', (payload) => {
-    dispatch(globalActions.removeChannel(payload.id))
-  });
-  socket.on('renameChannel', (payload) => {
-    dispatch(globalActions.renameChannel(payload))
-  });
+const dispatch = useDispatch();
+const socket = io();
+socket.on('newMessage', (payload) => {
+  dispatch(globalActions.addMessage(payload));
+  const messageBox = document.getElementById('messages-box');
+  messageBox.scrollTop = messageBox.scrollHeight;
+});
+socket.on('newChannel', (payload) => {
+  dispatch(globalActions.addChannel(payload));
+});
+socket.on('removeChannel', (payload) => {
+  dispatch(globalActions.removeChannel(payload.id));
+});
+socket.on('renameChannel', (payload) => {
+  dispatch(globalActions.renameChannel(payload));
+});
 
-  return (
-    <ContextProvider>
+return (
+  <ContextProvider>
     <div className='d-flex flex-column h-100'>
     <NavbarHeader />
 
@@ -65,8 +57,8 @@ const lang = useSelector((state) => state.langState.language);
     </BrowserRouter>
 
     </div>
-    </ContextProvider>
-  );
+  </ContextProvider>
+  )
 }
 
 export default App;
